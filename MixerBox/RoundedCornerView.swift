be4d96661e57695f9bacf41f9b1e0aa816e70297
Roberto Seidenberg <roberto.giessler@googlemail.com>
2017-05-30 15:46:00 +0200

    [BUGFIX] Fixes crash when initializing with frame CGRect.zero

--- MixerBox/RoundedCornerView.swift
+++ MixerBox/RoundedCornerView.swift
@@ -2,15 +2,10 @@ import UIKit
 
 class RoundedCornerView: UIView {
 
-  var borderWidth: CGFloat = 0 {
-    didSet {
-      innerBorderLayer.borderWidth = borderWidth
-      outerBorderLayer.borderWidth = borderWidth
-    }
-  }
+  var borderWidth: CGFloat = 0
 
-  var innerborderColor: UIColor = UIColor.clear { didSet { innerBorderLayer.borderColor = innerborderColor.cgColor }}
-  var outerborderColor: UIColor = UIColor.clear { didSet { outerBorderLayer.borderColor = outerborderColor.cgColor }}
+  var innerborderColor: UIColor = UIColor.clear
+  var outerborderColor: UIColor = UIColor.clear
 
   var innerBorderLayer: AnimationlessLayer!
   var outerBorderLayer: AnimationlessLayer!
@@ -37,23 +32,44 @@ class RoundedCornerView: UIView {
     innerBorderLayer.cornerRadius = min(bounds.size.width - borderWidth, bounds.size.height - borderWidth) / 6
     outerBorderLayer.cornerRadius = min(bounds.size.width, bounds.size.height) / 6
   }
-
-  override var bounds: CGRect { didSet { outerBorderLayer.frame = bounds }}
+  
+  override var bounds: CGRect {
+    didSet {
+      if setupSublayers() {
+        outerBorderLayer.frame = bounds
+      }
+    }
+  }
 
   func setup() {
     setupSublayers()
   }
 
-  func setupSublayers() {
-    innerBorderLayer = AnimationlessLayer()
-    outerBorderLayer = AnimationlessLayer()
-    layer.addSublayer(innerBorderLayer)
-    layer.addSublayer(outerBorderLayer)
+  @discardableResult func setupSublayers() -> Bool {
+    
+    if innerBorderLayer == nil && bounds != CGRect.zero {
+      
+      innerBorderLayer                 = AnimationlessLayer()
+      innerBorderLayer.borderWidth     = borderWidth
+      innerBorderLayer.backgroundColor = innerborderColor.cgColor
+      
+      outerBorderLayer                 = AnimationlessLayer()
+      outerBorderLayer.borderWidth     = borderWidth
+      outerBorderLayer.backgroundColor = outerborderColor.cgColor
+      
+      layer.addSublayer(innerBorderLayer)
+      layer.addSublayer(outerBorderLayer)
+      
+      return true
+      
+    } else {
+      return false
+    }
   }
 
   func updateSublayers() {
     
-    if let superview = superview {
+    if let superview = superview, innerBorderLayer != nil {
       let overlap = superview.frame.intersection(frame)
 
       let rx         = overlap.width / bounds.width
